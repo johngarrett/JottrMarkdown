e685d78dfdac15cae04bcdd66655ee8a23920aa2
John Garrett <jgarrett@kabbage.com>
2019-08-13 19:39:02 -0400

    framework creation

---
+++ JottrMarkdown/JottrMarkdown/JottrMarkdown.h
@@ -0,0 +1,19 @@
+//
+//  JottrMarkdown.h
+//  JottrMarkdown
+//
+//  Created by John Garrett on 8/13/19.
+//  Copyright © 2019 John Garrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JottrMarkdown.
+FOUNDATION_EXPORT double JottrMarkdownVersionNumber;
+
+//! Project version string for JottrMarkdown.
+FOUNDATION_EXPORT const unsigned char JottrMarkdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JottrMarkdown/PublicHeader.h>
+
+
